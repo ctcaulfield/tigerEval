7d9dad8ed66cb45032840c8e32dfaad0be1595e4
ISTE500Dev <iste500dev@iste-500teamcod.student.rit.edu>
2017-03-08 08:45:52 -0500

    Working copy of the importer for the IST Department to the test database

--- database/importer.py
+++ database/importer.py
@@ -2,13 +2,18 @@ import requests
 import simplejson
 import pymysql
 
-#conn = pymysql.connect(host='localhost',port=3306, user='root',password='student',db='catTest')
-#a = conn.cursor()
-
-currentTerm = '2165' # this will change or have to get past terms
+term = '2165' # this will change or have to get past terms
 termList = []
+
+counter = 0
+
+connection = pymysql.connect(host='localhost',
+				user='root',
+				password='student',
+				db='catTest',
+				cursorclass=pymysql.cursors.DictCursor)
+
 def getProfessors(url):
-	#get professors, put in list, return list
 	profList = []
 	r = requests.get(url)
 	c = r.content
@@ -20,24 +25,23 @@ def getProfessors(url):
 		email = item["email"]
 	return profList
 
-def executeCommand(query):
-#	a.execute(query)
-
-def getFromRITAPI(url):
-	r = requests.get(url)
-	c = r.content
-	j = simplejson.loads(c)
-	for item in j:
-		query = "insert into COURSE values('" + str(item["section"]) + "','" + str(item["term"]) + "','" + str(item["name"]) + "','" + str(item["instructors"][0]) + "','" + str(item["students"]) + "','GCCIS');"
-		return query
-
 profList = getProfessors("http://ist.rit.edu/api/people")
 for prof in profList:
 	url = "http://api.rit.edu/v1/faculty/"+prof+"/courses/&RITAuthorization=0a66ff145ca0ee53807a096eda307e5901801185"
-#	executeCommand(getFromRITAPI(url));
-	print getFromRITAPI(url));
-#a.commit()
-#a.close()
-#conn.close()
-
+	#url = "http://api.rit.edu/v1/faculty/"+prof+"/courses?term="+term+"&RITAuthorization=0a66ff145ca0ee53807a096eda307e5901801185"
+	reqs = requests.get(url)
+	content = reqs.content
+	json = simplejson.loads(content)
+	for item in json:
+		with connection.cursor() as cursor:
+			query = "insert into COURSE values('" + str(item["section"]) + "','" + str(item["term"]) + "','" +\
+				str(item["name"]) + "','" + str(item["instructors"][0]) + "','" + str(item["students"]) + "','GCCIS');"
+			cursor.execute(query)
+			connection.commit()
+			counter = counter + 1
+			print "Record for " + item['section'] + " inserted."
+			
+print "Inserted " + counter + " records into the database."
+connection.close()	
+#There are 290 courses being taught by 33 professors among the IST department
 
