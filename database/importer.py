031582b42109923a09bf5df0727ce9809294749f
ISTE500Dev <iste500dev@iste-500teamcod.student.rit.edu>
2017-03-07 22:59:03 -0500

    Not really sure honestly

--- database/importer.py
+++ database/importer.py
@@ -2,8 +2,8 @@ import requests
 import simplejson
 import pymysql
 
-conn = pymysql.connect(host='localhost',port=3306, user='root',password='student',db='catTest')
-a = conn.cursor()
+#conn = pymysql.connect(host='localhost',port=3306, user='root',password='student',db='catTest')
+#a = conn.cursor()
 
 currentTerm = '2165' # this will change or have to get past terms
 termList = []
@@ -21,22 +21,23 @@ def getProfessors(url):
 	return profList
 
 def executeCommand(query):
-	a.execute(query)
+#	a.execute(query)
 
 def getFromRITAPI(url):
 	r = requests.get(url)
 	c = r.content
 	j = simplejson.loads(c)
 	for item in j:
-		query = "insert into COURSE values('" + str(item["section"]) + "','" + str(item["term"]) + "','" + str(item["name"]) + "','" + str(item["instructors"][0]) + "','" + str(item["students"]) + "');"
+		query = "insert into COURSE values('" + str(item["section"]) + "','" + str(item["term"]) + "','" + str(item["name"]) + "','" + str(item["instructors"][0]) + "','" + str(item["students"]) + "','GCCIS');"
 		return query
 
 profList = getProfessors("http://ist.rit.edu/api/people")
 for prof in profList:
 	url = "http://api.rit.edu/v1/faculty/"+prof+"/courses/&RITAuthorization=0a66ff145ca0ee53807a096eda307e5901801185"
-	executeCommand(getFromRITAPI(url));
-a.commit()
-a.close()
-conn.close()
+#	executeCommand(getFromRITAPI(url));
+	print getFromRITAPI(url));
+#a.commit()
+#a.close()
+#conn.close()
 
 
