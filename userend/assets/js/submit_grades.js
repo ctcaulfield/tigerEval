c7fcaa59139a6f04fc75cfcef1875bc9233abbb0
ctcaulfield <ctcaulfield@gmail.com>
2017-03-13 19:17:21 -0400

    reget switch

--- userend/assets/js/submit_grades.js
+++ userend/assets/js/submit_grades.js
@@ -2,16 +2,22 @@ function validateForm(){
 	//validate if grades were properly pasted.
 	var gradeValues = $('textarea#gradesInput').val();
 	console.log(gradeValues);
-	//case 1 - separated by commas
-	if(/[0-9]{1,3}[,][0-9]{1,3}/.test(gradeValues)){
-		return true;
+
+	switch(gradeValues){
+		//case 1 - separated by commas
+		case (/^(\s*[0-9]{1,3}\s*[,]\s*[0-9]{1,3}\s*)+$/.test(gradeValues)):
+			
+			return true;
+
+		//case 2 - separated by spaces or line by line separation
+		case (/^(\s*[0-9]{1,3}\s*)+$/.test(gradeValues)):
+			
+			return true;
+
+		//default - if input not valid --> feedback
+		default:
+
+			return false;
+
 	}
-	//case 2 - separated by spaces
-
-	//case 3 - line by line separation
-
-	//checks if number of values is equal to provided number 
-	//of students
-
-
 }
