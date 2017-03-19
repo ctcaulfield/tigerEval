cc557def0b1a7cc44ae1f94aa2a2edeb5328830f
ctcaulfield <ctcaulfield@gmail.com>
2017-03-19 13:27:56 -0400

    changes to regex

--- userend/assets/js/submit_grades.js
+++ userend/assets/js/submit_grades.js
@@ -1,23 +1,31 @@
+//checks if the grades match the correct input format
+//
 function validateForm(){
 	//validate if grades were properly pasted.
 	var gradeValues = $('textarea#gradesInput').val();
-	console.log(gradeValues);
+	console.log("grade values: "+gradeValues);
 
-	switch(gradeValues){
-		//case 1 - separated by commas
-		case (/^(\s*[0-9]{1,3}\s*[,]\s*[0-9]{1,3}\s*)+$/.test(gradeValues)):
-			
-			return true;
-
-		//case 2 - separated by spaces or line by line separation
-		case (/^(\s*[0-9]{1,3}\s*)+$/.test(gradeValues)):
-			
-			return true;
-
-		//default - if input not valid --> feedback
-		default:
-
-			return false;
-
+	//comma separation?
+	if(/^[0-9]{1,3}(,[0-9]{1,3})*$/.test(gradeValues)){
+		console.log("valid input - commas");
+		return true;
+	}
+	//separated by spaces or line by line separation?
+	else if(/^(\s*[0-9]{1,3}\s*)+$/.test(gradeValues)){
+		console.log("valid input - line/line");
+		return true;
+	}
+	//did not follow the correct guideline for grade submission
+	else{
+		console.log("invalid");
+		return false;
 	}
 }
+
+//checks if the input values equals # students in course
+//checks if each value is within range of 0-100
+function correctValues(gradeValues){
+	
+
+}
+
